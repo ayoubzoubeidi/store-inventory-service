368269c60d35a38249ce74071f672b77351718ec
ayoub <ayoubzoubeidi@gmail.com>
2021-08-13 13:13:50 +0100

    initial project commit

---
+++ src/main/java/com/maz/store/inventory/StoreInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.maz.store.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreInventoryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maz/store/inventory/StoreInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maz.store.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
