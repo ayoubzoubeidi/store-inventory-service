60920d76aec62b8b0e951617894be57508d4a59a
Ayoub <ayoubzoubeidi@gmail.com>
2021-10-15 11:02:35 +0100

    misc changes and import optimizations

--- src/main/java/com/maz/store/inventory/web/controllers/InventoryController.java
+++ src/main/java/com/maz/store/inventory/web/controllers/InventoryController.java
@@ -2,7 +2,6 @@ package com.maz.store.inventory.web.controllers;
 
 import com.maz.store.inventory.service.InventoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
